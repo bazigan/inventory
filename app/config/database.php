a04685c0b1ad9c7539fe68716a970d53e05bdd6b
bazigan <bazigannetwork@gmail.com>
2025-06-12 22:34:54 +0700

    add dockerfile and compose

--- app/config/database.php
+++ app/config/database.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
+$host = 'db';
 $db   = 'inventory_db';
 $user = 'root';
-$pass = '';
+$pass = 'admin123';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
